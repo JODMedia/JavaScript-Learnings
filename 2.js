0d0e23b8cc73a3a359f91b080463111dcb42dff9
Jonas Dahlstrøm <jod.marketing.bureau@gmail.com>
2023-02-07 11:21:56 +0100

    eksterns js script

---
+++ 2.js
@@ -0,0 +1,9 @@
+var forNavn = "Jonas";
+var efterNavn = " Dahlstrøm";
+var fuldeNavn = forNavn + efterNavn;
+alert(fuldeNavn);
+
+var nummerEt = 7;
+var nummerTo = 56;
+var fuldeNummer = nummerEt + nummerTo;
+alert(fuldeNummer);
